63bc7410c63dfef647c2c286d4e370e0659e584b
i32petoj <i32petoj@uco.es>
2018-03-17 16:20:28 +0100

    Changed tweet

--- libs/tweet.py
+++ libs/tweet.py
@@ -3,9 +3,10 @@ import tweepy
 def tweet(text, api, idToReply = None, mediaURI = None):
     "Function for tweeting"
     #We have to split the text of the chat to tweet it
-    messageList = text.split()[1:]
-    messageText = str(messageList[0])
-    #Append the rest of the list to the string so we can call the tweet function
-    for i in messageList[1:]:
-        messageText = messageText + " " + str(i)
-    api.update_status(messageText, in_reply_to = idToReply, source = mediaURI)
+    if len(text) > 1: 
+        messageList = text.split()[1:]
+        messageText = str(messageList[0])
+        #Append the rest of the list to the string so we can call the tweet function
+        for i in messageList[1:]:
+            messageText = messageText + " " + str(i)
+        api.update_status(messageText, in_reply_to = idToReply, source = mediaURI)
