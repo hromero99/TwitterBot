766dd68c7df7d72d5e92f588daef53873231fbcd
i32petoj <i32petoj@uco.es>
2018-03-17 15:51:46 +0100

    Bot_functions stablished

--- libs/bot_functions.py
+++ libs/bot_functions.py
@@ -5,9 +5,10 @@ def getAPIObject(user_id):
     "Returns the api object for that user"
     with open("usuarios.json") as file: usuarios = json.load(file)
     listOfTokens = usuarios[str(user_id)]
-    auth = tweepy.OAuthHandler(str(listOfTokens[0]), str(listOfTokens[1]))
-    auth.set_access_token(str(listOfTokens[2]), str(listOfTokens[3]))
-
-    api = tweepy.API(auth)
-
+    api = 0
+    if len(listOfTokens) == 4:
+        auth = tweepy.OAuthHandler(str(listOfTokens[0]), str(listOfTokens[1]))
+        auth.set_access_token(str(listOfTokens[2]), str(listOfTokens[3]))
+        api = tweepy.API(auth)
+    
     return api
