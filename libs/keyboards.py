68096dca3fe58199fe72a35b830fafea52095ac5
Héctor Romero López <cyberh99@gmail.com>
2018-03-23 00:43:50 +0100

    Libs added

--- libs/keyboards.py
+++ libs/keyboards.py
@@ -1,3 +1,6 @@
+import telebot
+from telebot import *
+
 keyboard = types.InlineKeyboardMarkup()
 keyboard.add(types.InlineKeyboardButton('RT', callback_data='rt'),
              types.InlineKeyboardButton('Fav', callback_data='fav'))
